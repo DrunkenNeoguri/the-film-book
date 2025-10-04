988f0f84307ad541cd35409f50b7fc9a53c3d12e
DrunkenNeoguri <developneoguri@gmail.com>
2025-10-04 18:40:41 +0900

    fix: eslint 등 프로젝트 변경 사항에 따른 기타 수정 사항 반영

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-import { resolve } from "node:path";
+import { resolve } from 'node:path';
 
 //*MEMO: 차후에 배포 관련으로 좀 더 최적화할 수 있는지 확인 필요.
 export default defineConfig({
   plugins: [viteReact(), tailwindcss()],
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: 'jsdom',
   },
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      '@': resolve(__dirname, './src'),
     },
   },
 });
