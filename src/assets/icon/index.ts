2bd6fb4f69a18be9ada9ebddc72f531144bd7073
DrunkenNeoguri <developneoguri@gmail.com>
2025-10-07 01:12:10 +0900

    feat: svg 아이콘 에셋 추가 및 컴포넌트화

---
+++ src/assets/icon/index.ts
@@ -0,0 +1,6 @@
+import AddIcon from './icon-add.svg';
+import FavoriteIcon from './icon-favorite.svg';
+import MenuIcon from './icon-menu.svg';
+import ReportIcon from './icon-report.svg';
+
+export { AddIcon, FavoriteIcon, MenuIcon, ReportIcon };
