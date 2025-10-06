45ae08329f1f2d7ba1962c2cea28248a38a1ce13
DrunkenNeoguri <developneoguri@gmail.com>
2025-10-07 01:11:41 +0900

    feat: svg 파일을 컴포넌트화 할 수 있는 플러그인 라이브러리 설치 및 설정 적용

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-
 import { resolve } from 'node:path';
+import svgr from 'vite-plugin-svgr';
 
 //*MEMO: 차후에 배포 관련으로 좀 더 최적화할 수 있는지 확인 필요.
 export default defineConfig({
-  plugins: [viteReact(), tailwindcss()],
+  plugins: [viteReact(), tailwindcss(), svgr()],
   test: {
     globals: true,
     environment: 'jsdom',
