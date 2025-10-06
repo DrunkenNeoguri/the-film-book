a7dafa48dabe900c1a1101eb595a83db9f319b32
DrunkenNeoguri <developneoguri@gmail.com>
2025-10-06 14:55:11 +0900

    fix: 메인 페이지에 있던 section 요소 제거

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -3,11 +3,9 @@ import { MainBanner, MainDescription, LinkArea } from './components';
 export default function Main() {
   return (
     <main>
-      <section>
-        <MainBanner />
-        <MainDescription />
-        <LinkArea />
-      </section>
+      <MainBanner />
+      <MainDescription />
+      <LinkArea />
     </main>
   );
 }
