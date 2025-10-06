801595da56b2863cbd6554ce8dd1705aca741a61
DrunkenNeoguri <developneoguri@gmail.com>
2025-10-06 14:38:56 +0900

    fix: 버튼 내 스타일링 일부 수정

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -16,8 +16,8 @@ export type Props = ButtonProps | LinkProps;
 
 // *MEMO: Button으로 공용화하되, mode로 타입을 분리해서 쓰도록 정의.
 // *MEMO: Link에서 추가로 분리할 부분은 하단에 분리하도록 하고, 차후에 좀 더 리팩토링하여 간결화할 수 있다면 하도록 하자.
-export function Button({ mode, className, children, ...rest }: Props) {
-  const buttonStyle = 'flex px-4 py-2 rounded-l justify-center items-center w-full ' + className;
+export default function Button({ mode, className, children, ...rest }: Props) {
+  const buttonStyle = 'flex py-3 rounded-lg justify-center items-center w-full ' + className;
 
   if (mode === 'button') {
     return (
