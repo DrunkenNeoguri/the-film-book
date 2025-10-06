3f0757753aed57a9538aeac9c31d78bc3b4cbbdd
DrunkenNeoguri <developneoguri@gmail.com>
2025-10-06 14:47:02 +0900

    fix: LinkProps를 컴포넌트 props을 직접 받아 읽힐 수 있도록 타입 수정, rest 케이스도 분기 처리

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,11 +1,11 @@
 import { Link } from '@tanstack/react-router';
-import { ButtonHTMLAttributes, ReactNode } from 'react';
+import { ButtonHTMLAttributes, ComponentProps, ReactNode } from 'react';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   mode: 'button';
 };
 
-type LinkProps = typeof Link & {
+type LinkProps = ComponentProps<typeof Link> & {
   mode: 'link';
   className?: string;
   children?: ReactNode;
@@ -17,19 +17,21 @@ export type Props = ButtonProps | LinkProps;
 // *MEMO: Button으로 공용화하되, mode로 타입을 분리해서 쓰도록 정의.
 // *MEMO: Link에서 추가로 분리할 부분은 하단에 분리하도록 하고, 차후에 좀 더 리팩토링하여 간결화할 수 있다면 하도록 하자.
 export default function Button({ mode, className, children, ...rest }: Props) {
-  const buttonStyle = 'flex py-3 rounded-lg justify-center items-center w-full ' + className;
+  const buttonStyle = 'flex p rounded-lg justify-center items-center w-full ' + className;
 
   if (mode === 'button') {
+    const props = rest as ButtonProps;
+
     return (
-      <button className={buttonStyle} {...rest}>
+      <button className={buttonStyle} {...props}>
         {children}
       </button>
     );
   } else if (mode === 'link') {
-    const { to } = { ...rest } as LinkProps;
+    const props = rest as LinkProps;
 
     return (
-      <Link className={buttonStyle} to={to}>
+      <Link className={buttonStyle} {...props}>
         {children}
       </Link>
     );
