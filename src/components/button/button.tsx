900626a8698d196e2af53ca957f58219f1ddeb04
DrunkenNeoguri <developneoguri@gmail.com>
2025-10-07 01:12:28 +0900

    fix: 버튼 컴포넌트에 상하단 패딩 누락되어 있던 부분 수정

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -14,10 +14,10 @@ type LinkProps = ComponentProps<typeof Link> & {
 
 export type Props = ButtonProps | LinkProps;
 
-// *MEMO: Button으로 공용화하되, mode로 타입을 분리해서 쓰도록 정의.
+// *MEMO: Button으로 공용화하되, mode로 타입을 분리해서 쓰도록 정의
 // *MEMO: Link에서 추가로 분리할 부분은 하단에 분리하도록 하고, 차후에 좀 더 리팩토링하여 간결화할 수 있다면 하도록 하자.
 export default function Button({ mode, className, children, ...rest }: Props) {
-  const buttonStyle = 'flex p rounded-lg justify-center items-center w-full ' + className;
+  const buttonStyle = 'flex py-3 rounded-lg justify-center items-center w-full ' + className;
 
   if (mode === 'button') {
     const props = rest as ButtonProps;
