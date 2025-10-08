1046c730850c425c93f1784d4d3a3872ea45e52d
DrunkenNeoguri <developneoguri@gmail.com>
2025-10-08 22:54:05 +0900

    fix: ImageUploader에 아이콘 컴포넌트 추가

--- src/components/image-uploader/image-uploader.tsx
+++ src/components/image-uploader/image-uploader.tsx
@@ -1,6 +1,7 @@
 //*MEMO: 기능 역할과 디자인은 서로 별개로 둘 것.
 //*MEMO: 그런 의미에서 외부에서 기능 사항을 받아서 반영할 것.
 
+import { AddIcon } from '../../assets/icon';
 import { MONTHS } from '../../utils/constants';
 
 type Props = {
@@ -10,9 +11,10 @@ type Props = {
 export default function ImageUploader({ month }: Props) {
   return (
     <div className="flex relative w-full h-36 border-amber-400 border-4 rounded-xl">
-      {/* 해당 영역에서 이미지가 나오도록 함 */}
-      <label htmlFor={month}></label>
-      <input id={month} type="file" />
+      <label htmlFor={month} className="flex justify-around items-center cursor-pointer w-full">
+        <AddIcon width={48} height={48} className="mb-2" />
+      </label>
+      <input id={month} type="file" className="hidden" />
       <div className="flex absolute bottom-0 w-full justify-center items-center rounded-b-lg bg-amber-400 text-[#19181D] text-semibold text-base">
         <span>{month}</span>
       </div>
