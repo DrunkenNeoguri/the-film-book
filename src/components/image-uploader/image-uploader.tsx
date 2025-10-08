47de8e18db755495460b04aa51d06cb129fc2579
DrunkenNeoguri <developneoguri@gmail.com>
2025-10-09 06:15:07 +0900

    fix: 훅 배치 및 외부에서 파일을 받아갈 내용 보충

--- src/components/image-uploader/image-uploader.tsx
+++ src/components/image-uploader/image-uploader.tsx
@@ -1,21 +1,45 @@
-//*MEMO: 기능 역할과 디자인은 서로 별개로 둘 것.
-//*MEMO: 그런 의미에서 외부에서 기능 사항을 받아서 반영할 것.
-
+import { Activity, ChangeEvent } from 'react';
 import { AddIcon } from '../../assets/icon';
 import { MONTHS } from '../../utils/constants';
+import { useImageUpload } from './use-image-upload';
 
+// *MEMO: 전체에서 이미지 리스트를 가져올 사항이 필요해보인다.
 type Props = {
   month: (typeof MONTHS)[number];
+  existingImage?: File;
+  existingImageUrl?: string;
+  onUpload: (file: File) => void;
 };
 
-export default function ImageUploader({ month }: Props) {
+export default function ImageUploader({ month, existingImage, existingImageUrl, onUpload }: Props) {
+  const { previewImageUrl, loadImageFile } = useImageUpload({
+    existingImage,
+    existingImageUrl,
+  });
+
+  const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    const selectedImage = e.target.files?.[0];
+    if (!selectedImage) {
+      return;
+    }
+
+    loadImageFile(selectedImage);
+    onUpload(selectedImage);
+  };
+
   return (
     <div className="flex relative w-full h-36 border-amber-400 border-4 rounded-xl">
       <label htmlFor={month} className="flex justify-around items-center cursor-pointer w-full">
-        <AddIcon width={48} height={48} className="mb-2" />
+        <Activity mode={!previewImageUrl ? 'visible' : 'hidden'}>
+          <AddIcon width={48} height={48} className="mb-2" />
+        </Activity>
+        <Activity mode={previewImageUrl ? 'visible' : 'hidden'}>
+          <img src={previewImageUrl} />
+        </Activity>
       </label>
       <input
         id={month}
+        onChange={handleImageUpload}
         type="file"
         accept="image/jpeg, image/jpg, image/png, image/webp"
         className="hidden"
