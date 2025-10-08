e285b2676634ec8e02ced75bc251ae1baa32fae7
DrunkenNeoguri <developneoguri@gmail.com>
2025-10-08 22:55:25 +0900

    fix: layout 컴포넌트 반영

--- src/pages/film/develop/edit/index.tsx
+++ src/pages/film/develop/edit/index.tsx
@@ -1,14 +1,17 @@
 import { useParams } from '@tanstack/react-router';
 import { ButtonArea } from '../components/button-area';
 import PhotoList from '../components/photo-list';
+import { DevelopLayout } from '../layout';
 
-export default function Edit({ params }) {
+export default function Edit({ params }: { params: any }) {
   const { filmId } = useParams(params);
   return (
-    <section>
-      <p>수정할 사진이 있다면 관련 내용을 클릭해서 수정해주세요.</p>
+    <DevelopLayout>
+      <p className="text-gray-50 text-base px-4 py-6">
+        수정할 사진이 있다면 관련 내용을 클릭해서 수정해주세요.
+      </p>
       <PhotoList />
       <ButtonArea />
-    </section>
+    </DevelopLayout>
   );
 }
--- src/pages/film/develop/layout.tsx
+++ src/pages/film/develop/layout.tsx
@@ -3,11 +3,11 @@ import { PropsWithChildren } from 'react';
 export function DevelopLayout({ children }: PropsWithChildren) {
   return (
     // *MEMO: 웹에서도 inset 처리가 가능하다면 해당 부분 참고해서 헤더 영억 height 조정할 것
-    <section className="flex flex-col w-full h-full pb-20 relative overflow-scroll">
+    <section className="flex flex-col w-full h-full relative">
       <div className="flex w-full h-12 bg-[#121212] text-gray-50 text-base font-semibold justify-center items-center">
         필름 내용 업로드 / 수정하기
       </div>
-      {children}
+      <div className="flex-1 overflow-y-auto pb-20">{children}</div>
     </section>
   );
 }
