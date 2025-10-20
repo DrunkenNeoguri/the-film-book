c09ae3ad780d038b469bab973f2e44ab0eb0abe9
DrunkenNeoguri <developneoguri@gmail.com>
2025-10-20 23:45:45 +0900

    feat: 필름 상세 페이지 초안 추가

--- src/pages/film/detail/index.tsx
+++ src/pages/film/detail/index.tsx
@@ -1,3 +1,14 @@
+//*MEMO: 필요한 것들?
+//*MEMO: 필름 영역, 클릭할 때의 폴라로이드 모달, 좋아요 버튼, 신고 버튼 정도만.
+//*MEMO: 사진 못 퍼가게 마우스 우클릭 막을 것. (그래도 퍼갈 사람은 퍼가겠지만...)
+
+import { FABArea, FilmBase } from './components';
+
 export default function Detail() {
-  return <section>detail</section>;
+  return (
+    <section className="relative">
+      <FilmBase />
+      <FABArea />
+    </section>
+  );
 }
